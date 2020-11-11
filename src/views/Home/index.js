3bb3595a5af4e597a189deb351e6237e92b1f04b
Sergio Henrique <sergionavire@gmail.com>
2020-11-11 20:57:30 -0300

    importacao inicial

---
+++ src/views/Home/index.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import { View, Text, TouchableOpacity, ScrollView } from 'react-native';
+
+import styles from './styles';
+
+// COMPONENTES
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import TaskCard from '../../components/TaskCard';
+
+export default function Home(){
+
+  const[filter, SetFilter] = useState('today');
+
+  return (
+    <View style={styles.container}>
+      <Header showNotification={true} showBack={true} />
+
+      <View style={styles.filter}>
+
+      <TouchableOpacity onPress={() => SetFilter('all')}>
+        <Text style={filter == 'all' ? styles.filterTextActived : styles.filterTextInative}>Todos</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => SetFilter('today')}>
+        <Text style={filter == 'today' ? styles.filterTextActived : styles.filterTextInative}>Hoje</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => SetFilter('month')}>
+        <Text style={filter == 'month' ? styles.filterTextActived : styles.filterTextInative}>Mês</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => SetFilter('week')}>
+        <Text style={filter == 'week' ? styles.filterTextActived : styles.filterTextInative}>Semana</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => SetFilter('year')}>
+        <Text style={filter == 'year' ? styles.filterTextActived : styles.filterTextInative}>Ano</Text>
+      </TouchableOpacity>
+
+      </View>
+
+      <View style={styles.title}>
+        <Text style={styles.titleText}>TAREFAS</Text>
+      </View>
+
+      <ScrollView style={styles.content} contentContainerStyle={ alignItems='center' }>
+        <TaskCard done={true} />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+      </ScrollView>
+
+      <Footer icon={'add'} />
+    </View>
+  );
+};
